603754fdfb8eb0469745e9ebfced1b9391979f15
Daniel J. Summers <daniel@bitbadger.solutions>
2020-09-05 21:45:25 -0400

    Initial preview site

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # jobs-jobs-jobs
+
+Repository for the development of No Agenda Jobs - currently parked [here](http://jobs.bitbadger.solutions)
---
+++ src/jobs-jobs-jobs/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <Home msg="this message is not displayed"/>
+  </div>
+</template>
+
+<script lang="ts">
+import Home from './components/Home.vue'
+
+export default {
+  components: {
+    Home,
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
