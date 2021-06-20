73d6d0df944df797525c2b02c97565e0c7858f67
Daniel J. Summers <daniel@bitbadger.solutions>
2021-06-19 22:49:50 -0400

    Add WIP for listing page (#16)

---
+++ src/JobsJobsJobs/Client/Pages/Listings/Mine.razor.cs
@@ -0,0 +1,41 @@
+﻿using JobsJobsJobs.Shared;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JobsJobsJobs.Client.Pages.Listings
+{
+    public partial class Mine : ComponentBase
+    {
+        /// <summary>
+        /// Whether the page is loading data
+        /// </summary>
+        private bool Loading { get; set; } = true;
+
+        /// <summary>
+        /// Error messages encountered while searching for profiles
+        /// </summary>
+        private IList<string> ErrorMessages { get; } = new List<string>();
+
+        /// <summary>
+        /// The job listings entered by the current user
+        /// </summary>
+        private IEnumerable<Listing> Listings { get; set; } = Enumerable.Empty<Listing>();
+
+        protected override async Task OnInitializedAsync()
+        {
+            var listings = await ServerApi.RetrieveMany<Listing>(http, "listing/mine");
+
+            if (listings.IsOk)
+            {
+                Listings = listings.Ok;
+            }
+            else
+            {
+                ErrorMessages.Add(listings.Error);
+            }
+        }
+    }
+}
---
+++ src/JobsJobsJobs/Server/Areas/Api/Controllers/ListingController.cs
@@ -0,0 +1,52 @@
+﻿using JobsJobsJobs.Server.Data;
+using JobsJobsJobs.Shared;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using NodaTime;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace JobsJobsJobs.Server.Areas.Api.Controllers
+{
+    /// <summary>
+    /// API controller for job listings
+    /// </summary>
+    [Route("api/listings")]
+    [Authorize]
+    [ApiController]
+    public class ListingController : ControllerBase
+    {
+        /// <summary>
+        /// The data context
+        /// </summary>
+        private readonly JobsDbContext _db;
+
+        /// <summary>
+        /// The NodaTime clock instance
+        /// </summary>
+        private readonly IClock _clock;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="db">The data context to use for this request</param>
+        /// <param name="clock">The clock instance to use for this request</param>
+        public ListingController(JobsDbContext db, IClock clock)
+        {
+            _db = db;
+            _clock = clock;
+        }
+
+        /// <summary>
+        /// The current citizen ID
+        /// </summary>
+        private CitizenId CurrentCitizenId => CitizenId.Parse(User.FindFirst(ClaimTypes.NameIdentifier)!.Value);
+
+        [HttpGet("mine")]
+        public async Task<IActionResult> Mine() =>
+            Ok(await _db.FindListingsByCitizen(CurrentCitizenId));
+    }
+}
