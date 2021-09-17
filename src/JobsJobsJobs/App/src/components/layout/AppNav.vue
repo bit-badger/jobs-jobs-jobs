bc0531a9fcc7c72953ebbc951fceadd92631b9af
Daniel J. Summers <daniel@bitbadger.solutions>
2021-09-16 21:53:34 -0400

    Add mobile menu/layout

--- src/JobsJobsJobs/App/src/components/layout/AppNav.vue
+++ src/JobsJobsJobs/App/src/components/layout/AppNav.vue
@@ -1,88 +1,41 @@
 <template lang="pug">
-aside.collapse.show.p-3
+#mobileMenu.offcanvas.offcanvas-end(v-if="showMobileMenu" tabindex="-1" aria-labelledby="mobileMenuLabel")
+  .offcanvas-header
+    h5#mobileMenuLabel Menu
+    button.btn-close.text-reset(type="button" data-bs-dismiss="offcanvas" aria-label="Close")
+  .offcanvas-body: app-links
+aside.collapse.show.p-3(v-else)
   p.home-link.pb-3: router-link(to="/") Jobs, Jobs, Jobs
   p &nbsp;
-  nav
-    template(v-if="isLoggedOn")
-      router-link(to="/citizen/dashboard") #[icon(:icon="mdiViewDashboardVariant")]&nbsp; Dashboard
-      router-link(to="/help-wanted") #[icon(:icon="mdiNewspaperVariantMultipleOutline")]&nbsp; Help Wanted!
-      router-link(to="/profile/search") #[icon(:icon="mdiViewListOutline")]&nbsp; Employment Profiles
-      router-link(to="/success-story/list") #[icon(:icon="mdiThumbUp")]&nbsp; Success Stories
-      .separator
-      router-link(to="/listings/mine") #[icon(:icon="mdiSignText")]&nbsp; My Job Listings
-      router-link(to="/citizen/profile") #[icon(:icon="mdiPencil")]&nbsp; My Employment Profile
-      .separator
-      router-link(to="/citizen/log-off") #[icon(:icon="mdiLogoutVariant")]&nbsp; Log Off
-    template(v-else)
-      router-link(to="/") #[icon(:icon="mdiHome")]&nbsp; Home
-      router-link(to="/profile/seeking") #[icon(:icon="mdiViewListOutline")]&nbsp; Job Seekers
-      router-link(to="/citizen/log-on") #[icon(:icon="mdiLoginVariant")]&nbsp; Log On
-    router-link(to="/how-it-works") #[icon(:icon="mdiHelpCircleOutline")]&nbsp; How It Works
+  app-links
 </template>
 
 <script setup lang="ts">
-import { computed } from "vue"
-import { useStore } from "@/store"
-import {
-  mdiHelpCircleOutline,
-  mdiHome,
-  mdiLoginVariant,
-  mdiLogoutVariant,
-  mdiNewspaperVariantMultipleOutline,
-  mdiPencil,
-  mdiSignText,
-  mdiThumbUp,
-  mdiViewDashboardVariant,
-  mdiViewListOutline
-} from "@mdi/js"
+import { useBreakpoints, breakpointsBootstrapV5 } from "@vueuse/core"
+import AppLinks from "./AppLinks.vue"
 
-const store = useStore()
+const breakpoints = useBreakpoints(breakpointsBootstrapV5)
 
-/** Whether a user is logged in or not */
-const isLoggedOn = computed(() => store.state.user !== undefined)
+/** Whether the mobile menu or the desktop menu should be shown */
+const showMobileMenu = breakpoints.smaller("md")
 </script>
 
 <style lang="sass" scoped>
-aside
+aside,
+#mobileMenu
   background-image: linear-gradient(180deg, darkgreen 0%, green 70%)
   color: white
   font-size: 1.2rem
+aside
   min-height: 100vh
   width: 250px
   min-width: 250px
   position: sticky
   top: 0
-path
-  fill: white
-path:hover
-  fill: black
-a:link, a:visited
-  text-decoration: none
-  color: white
-  // font-weight: 500
 .home-link
   font-size: 1.2rem
   text-align: center
   background-color: rgba(0, 0, 0, .4)
   margin: -1rem
   padding: 1rem
-nav > a
-  display: block
-  width: 100%
-  border-radius: .25rem
-  padding: .5rem
-  margin: .5rem 0
-  font-size: 1rem
-  > i
-    vertical-align: top
-    margin-right: 1rem
-  &.router-link-exact-active
-    background-color: rgba(255, 255, 255, .2)
-  &:hover
-    background-color: rgba(255, 255, 255, .5)
-    color: black
-    text-decoration: none
-nav > div.separator
-  border-bottom: solid 1px rgba(255, 255, 255, .75)
-  height: 1px
 </style>
--- src/JobsJobsJobs/App/src/components/layout/TitleBar.vue
+++ src/JobsJobsJobs/App/src/components/layout/TitleBar.vue
@@ -1,9 +1,9 @@
 <template lang="pug">
-nav.navbar.navbar-dark.mobile
+nav.navbar.navbar-dark(v-if="showMobileHeader")
   span.navbar-text: router-link(to="/") Jobs, Jobs, Jobs
   button.btn(data-bs-toggle="offcanvas" data-bs-target="#mobileMenu" aria-controls="mobileMenu")
     icon(:icon="mdiMenu")
-nav.navbar.navbar-light.bg-light.wide
+nav.navbar.navbar-light.bg-light(v-else)
   span &nbsp;
   span.navbar-text.
     (&hellip;and Jobs &ndash; #[audio-clip(clip="pelosi-jobs") Let&rsquo;s Vote for Jobs!])
@@ -11,24 +11,28 @@ nav.navbar.navbar-light.bg-light.wide
 
 <script setup lang="ts">
 import { mdiMenu } from "@mdi/js"
+import { useBreakpoints, breakpointsBootstrapV5 } from "@vueuse/core"
+
 import AudioClip from "@/components/AudioClip.vue"
+
+const breakpoints = useBreakpoints(breakpointsBootstrapV5)
+
+/** Whether to show the mobile or desktop header */
+const showMobileHeader = breakpoints.smaller("md")
 </script>
 
 <style lang="sass" scoped>
-.mobile
+.navbar-dark
   background-image: linear-gradient(0deg, green 0%, darkgreen 70%)
+  padding-left: 1rem
+  padding-right: 1rem
   button
-    padding: 0 1rem 0 0
+    padding: 0
   .navbar-text
     font-weight: bold
     color: white
-.wide
-  display: none
-@media (min-width: 768px)
-  .wide
-    display: flex
-  .mobile
-    display: none
+.navbar-light
   .navbar-text
     font-style: italic
+    padding: 0 1rem 0 0
 </style>
