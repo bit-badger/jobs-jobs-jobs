683506d7354c7e070b5bd1190dc1fa02fbf9ed9e
Daniel J. Summers <daniel@bitbadger.solutions>
2023-01-16 20:23:58 -0500

    Remove Vue app

--- src/JobsJobsJobs/App/src/store/actions.ts
+++
@@ -1,5 +0,0 @@
-/** Logs a user on to Jobs, Jobs, Jobs */
-export const LogOn = "logOn"
-
-/** Ensures that the continent list in the state has been populated */
-export const EnsureContinents = "ensureContinents"
